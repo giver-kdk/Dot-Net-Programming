15591010a08858b618966a67b3b4512f65a06d30
giver-kdk <giverbro13@gmail.com>
2024-02-04 22:47:51 +0545

    Last VS-Code CSharp

--- 01-C-Sharp/01-Basics/08-Pass-by-Reference.cs
+++ 01-C-Sharp/01-Basics/08-Pass-by-Reference.cs
@@ -8,12 +8,22 @@ namespace PassRef
 {
 	public class Program
 	{
-		// Method taking reference of variable as parameter
+		// Using 'ref'
 		static void Modify(ref int i)
 		{
 			i *= 2;
 			Console.WriteLine("Modified i = {0}", i);
 		}
+		// 'out' used when method returns multiple values and 
+		// Variable can declared on time of calling the method
+		public static string Increment(out int num)
+		{
+			num = 1;		// Needs to be initialized inside method
+			num++;
+			return String.Format("Increment is {0}", num);
+		} 
+		// Using 'ref readonly'
+		// Using 'in'
 		static void Main(string[] args)
 		{
 			int i = 10;
@@ -21,6 +31,9 @@ namespace PassRef
 			// Passing reference of variable as argument
 			Modify(ref i);
 			Console.WriteLine("After Modification,  i = {0}", i);
+			// int n;			// No need to initialize for using 'out'
+			string result = Increment(out int n);
+			Console.WriteLine(result);
 		}
 	}
 }
--- 01-C-Sharp/04-Intermediate/ConsoleApp/Program.cs
+++ 01-C-Sharp/04-Intermediate/ConsoleApp/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Text;
+﻿/*
+	- We know 'reference types' makes the original variable modified anyhow. 
+	- To modify original variable of 'value type', use 'ref' or 'out' keyword
+*/
+using System;
 
-namespace StrigBuilder
+namespace PassRef
 {
 	public class Program
 	{
+		// Using 'ref'
+		static void Modify(ref int i)
+		{
+			i *= 2;
+			Console.WriteLine("Modified i = {0}", i);
+		}
+		// 'out' used when method returns multiple values and 
+		// Variable can declared on time of calling the method
+		public static string Increment(out int num)
+		{
+			num = 1;        // Needs to be initialized inside method
+			num++;
+			return String.Format("Increment is {0}", num);
+		}
+		// Using 'ref readonly'
+		// Using 'in'
 		static void Main(string[] args)
 		{
-			// 'StringBuilder' is a class
-			StringBuilder sb = new StringBuilder("Hello.");
-			Console.WriteLine(sb.ToString());
+			int i = 10;
+			Console.WriteLine("Before Modification,  i = {0}", i);
+			// Passing reference of variable as argument
+			Modify(ref i);
+			Console.WriteLine("After Modification,  i = {0}", i);
+			// int n;			// No need to initialize for using 'out'
+			string result = Increment(out int n);
+			Console.WriteLine(result);
 		}
 	}
 }
