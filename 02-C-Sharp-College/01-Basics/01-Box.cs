7e6b026a30ad88b59c7582434dec8fd602c6b762
Revig <giverbro13@gmail.com>
2024-02-07 16:27:30 +0545

    box update 1

--- 02-C-Sharp-College/01-Basics/01-Box.cs
+++ 02-C-Sharp-College/01-Basics/01-Box.cs
@@ -6,9 +6,27 @@ namespace Displayer
     public class Box
     {
         public double length, breadth, height;
+        // Compiler makes default constructor if no constructor created
+        // That Default constructor gives default value to fields
+        // If number, then '0' assigned
+        // If string/reference type, then 'null' assigned
+        
+        // Non-Parameterized Constructor (Some call it Default Constructor)
+        public Box()
+        {
+            length = 5.5;
+            breadth = 5.5;
+            height = 5.5;
+        }
         public void Display()
         {
             Console.WriteLine("Length: " + length + "\nBreadth: " + breadth + "\nHeight: " + height);
+        }
+        public void SetDimension(double l, double b, double h)
+        {
+            length = l;
+            breadth = b;
+            height = h;
         }
     }
     public class Test
@@ -24,6 +42,24 @@ namespace Displayer
             b1.breadth = 10;
             b1.height = 10;
             b1.Display();
+
+            Console.WriteLine();
+
+            Box b2;             // This is not an object now. This is a reference variable
+            b2 = new Box();     // Now, b2 is a object(which we know is a reference type)
+            b2.SetDimension(20, 20, 20);
+            b2.Display();
+
+            Console.WriteLine();
+
+            // Another way to create object
+            Box b3 = new Box()
+            {
+                length = 30,
+                breadth = 30,
+                height = 30
+            };
+            b3.Display();
         }
     }
 }
