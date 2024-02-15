6a057c50346de9adf2d3afc7d9c17a80f62ad425
Revig <giverbro13@gmail.com>
2024-02-15 16:09:28 +0545

    indexer update

--- 02-C-Sharp-College/01-Basics/10-Indexer.cs
+++ 02-C-Sharp-College/01-Basics/10-Indexer.cs
@@ -6,9 +6,9 @@ namespace Indexer
     public class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+        public string Name { get; set; }
         public double Salary { get; set; }
-        public string Department { get; set; }  
+        public string Department { get; set; }
 
         public Employee()
         {
@@ -38,10 +38,28 @@ namespace Indexer
             }
             set
             {
-                if (i == 0) this.Id = (int) value;
-                else if (i == 1) this.Name = (string) value;
-                else if (i == 2) this.Salary = (double) value;
-                else if (i == 3) this.Department = (string) value;
+                if (i == 0) this.Id = (int)value;
+                else if (i == 1) this.Name = (string)value;
+                else if (i == 2) this.Salary = (double)value;
+                else if (i == 3) this.Department = (string)value;
+            }
+        }
+        public object this[string s]
+        {
+            get
+            {
+                if (s == "id") return this.Id;
+                else if (s == "name") return this.Name;
+                else if (s == "salary") return this.Salary;
+                else if (s == "department") return this.Department;
+                else return "Invalid Index";
+            }
+            set
+            {
+                if (s == "id") this.Id = (int)value;
+                else if (s == "name") this.Name = (string)value;
+                else if (s == "salary") this.Salary = (double)value;
+                else if (s == "department") this.Department = (string)value;
             }
         }
     }
@@ -49,17 +67,24 @@ namespace Indexer
     {
         public static void Main(string[] args)
         {
-            Employee emp = new Employee(123, "Ram", 90000, "IT");
+            Employee emp = new Employee(123, "Ram", 90000.0d, "IT");
 
             emp[0] = 456;
             emp[1] = "Hari";
-            emp[2] = 100000;
+            emp[2] = 100000.0d;
             emp[3] = "Physics";
 
             Console.WriteLine(emp[0]);
             Console.WriteLine(emp[1]);
             Console.WriteLine(emp[2]);
             Console.WriteLine(emp[3]);
+
+            // Using indexer with non-interger type index
+            Console.WriteLine();
+            Console.WriteLine(emp["id"]);
+            Console.WriteLine(emp["name"]);
+            Console.WriteLine(emp["salary"]);
+            Console.WriteLine(emp["department"]);
         }
     }
 }
