ae87d188c7aee4ce3d072e21b31e418b213abf64
Revig <giverbro13@gmail.com>
2024-02-21 16:37:47 +0545

    Method Overriding

---
+++ 02-C-Sharp-College/01-Basics/11-Method-Overriding.cs
@@ -0,0 +1,55 @@
+
+
+
+// 'Object' class is the super class of all the classes. 
+using System;
+
+namespace InheritanceWAP
+{
+    public class Parent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Parent(int i, string n)
+        {
+            Id = i;
+            Name = n;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("I am Parent");
+        }
+    }
+    public class Child : Parent
+    {
+        public int Age { get; set; }
+
+        public Child(int i, string n, int a) : base(i, n)
+        {
+            Age = a;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("I am Child");
+            Console.WriteLine("Age is: " + Age);
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // We can use parent reference variable to store child instance. But vice-versa is not true
+            Parent p1 = new Parent(123, "Ram");
+            Console.WriteLine(p1.Id);         // Can access parent's members
+            Console.WriteLine(p1.Name);         // Can access parent's members
+            // Console.WriteLine(p1.Age);       // Can't access child's members
+
+            p1.Display();                       // Can run the overridden method
+
+            Child c1 = new Child(234, "Shyam", 16);
+            p1 = c1;
+            p1.Display();
+        }
+    }
+}
