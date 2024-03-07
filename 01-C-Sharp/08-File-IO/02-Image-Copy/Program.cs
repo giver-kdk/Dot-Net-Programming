393047d4e5af3bd7e0de2906e967e9825f2e5201
giver-kdk <giverbro13@gmail.com>
2024-03-07 16:36:29 +0545

    Image Copy

--- 01-C-Sharp/08-File-IO/02-Image-Copy/Program.cs
+++ 01-C-Sharp/08-File-IO/02-Image-Copy/Program.cs
@@ -6,7 +6,9 @@ namespace ImageCopyProgram
 	{
 		public static void Main(string[] args)	
 		{
+			// File path of source file
 			string srcFilePath = @"D:\CODES\Dot-Net-Development\01-C-Sharp\ConsoleProgram\Sky.jpg";
+			// File path of destination file
 			string destFilePath = @"D:\CODES\Dot-Net-Development\01-C-Sharp\ConsoleProgram\Hill.jpg";
 
 			// 'Copy(Source, Destination, Overwrite)'
