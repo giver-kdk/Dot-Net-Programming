deb9007b1009bbf88d184dc127af34adfb989e9d
giver-kdk <giverbro13@gmail.com>
2024-03-29 09:34:48 +0545

    DI

--- 03-ASP-Dot-NET/02-College-Web-Apps/02-Student-Web-App/WebApplication2/Models/StudentModel.cs
+++ 03-ASP-Dot-NET/02-College-Web-Apps/02-Student-Web-App/WebApplication2/Models/StudentModel.cs
@@ -1,7 +1,23 @@
-﻿namespace WebApplication2.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;            // For model validation attributes
+
+namespace WebApplication2.Models
 {
 	public class StudentModel
 	{
-		
+		// Model Validation. The model is annotated after this
+		[Required(ErrorMessage = "Id must be passed")]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(50, MinimumLength = 20, ErrorMessage = "Name length must be 2 to 50")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Age is required")]
+		[Range(1, 17, ErrorMessage = "Age must be between 1 to 17")]
+		public int Age { get; set; }
+		[Required(ErrorMessage = "Email is required")]
+		public string Email { get; set; }
 	}
 }
