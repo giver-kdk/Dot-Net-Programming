0f36bb8f8d3355360596108b41a87ec0db100cc9
Revig <giverbro13@gmail.com>
2024-04-15 14:27:19 +0545

    Controller

--- 03-ASP-Dot-NET/02-College-Web-Apps/07-ORM-App-Ref/Controller/ProductController.cs
+++ 03-ASP-Dot-NET/02-College-Web-Apps/07-ORM-App-Ref/Controller/ProductController.cs
@@ -1,12 +1,100 @@
+using _06_ORM_App.Models;
+using _06_ORM_App.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _06_ORM_App.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        // ****** Dependency Injection of ProductRepo ******
+        ProductRepo _repo = null;
+        public ProductController(ProductRepo repo)
+        {
+            _repo = repo;
+        }
+        // GET: ProductController
+        public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            // ******  ******
+            return View(_repo.GetSingleRecord(id));
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product prod)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _repo.AddRecord(prod);
+                    return Content("Record has been inserted");
+                }
+                else
+                {
+                    return View(prod);
+                }
+            }
+            catch(Exception ex)
+            {
+                return Content("Something went wrong when inserting record: " + ex.Message);
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
