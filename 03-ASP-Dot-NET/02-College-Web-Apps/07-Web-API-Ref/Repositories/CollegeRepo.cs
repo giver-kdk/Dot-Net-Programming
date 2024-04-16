ccf101deb1c8aa91992db12569821557dcc4580c
Revig <giverbro13@gmail.com>
2024-04-16 13:36:17 +0545

    College Repo

---
+++ 03-ASP-Dot-NET/02-College-Web-Apps/07-Web-API-Ref/Repositories/CollegeRepo.cs
@@ -0,0 +1,32 @@
+using _07_Web_API.Models;
+
+namespace _07_Web_API.Repositories
+{
+    public class CollegeRepo : IRepository<College>
+    {
+        public void AddRecord(College model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRecord(College model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<College> GetAllRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public College GetSingleRecord(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public College UpdateRecord(College model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 03-ASP-Dot-NET/02-College-Web-Apps/07-Web-API-Ref/Repositories/IRepository.cs
+++ 03-ASP-Dot-NET/02-College-Web-Apps/07-Web-API-Ref/Repositories/IRepository.cs
@@ -1,7 +1,17 @@
 namespace _07_Web_API.Repositories
 {
-    public class IRepository
+    // Create an interface to create a standard for large project
+    public interface IRepository<T>
     {
-
+        // 'AddRecord()' returns void because there is no data manipulation
+        public void AddRecord(T model);
+        // 'GetAllRecords()' returns 'List<T>' because there is data fetch and needs to be returned
+        public List<T> GetAllRecords();
+        // 'UpdateRecord()' returns 'T' because there is data fetch and needs to be returned
+        public T GetSingleRecord(int id);
+        // 'UpdateRecord()' returns 'T' because there is data manipulation
+        public T UpdateRecord(T model);
+        // 'UpdateRecord()' returns void because there is no data manipulation
+        public void DeleteRecord(T model);
     }
 }
