e030b46c76e54b19fcb1350496fe31ca5d0acd2a
giver-kdk <giverbro13@gmail.com>
2024-05-20 15:50:51 +0545

    API Request in Console

--- 01-C-Sharp/ConsoleProgram/Program.cs
+++ 01-C-Sharp/ConsoleProgram/Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace AttributeApp
+class Program
 {
-    [AttributeUsage(
-    AttributeTargets.Class |
-    AttributeTargets.Struct |
-    AttributeTargets.Constructor |
-    AttributeTargets.Field |
-    AttributeTargets.Property |
-    AttributeTargets.Method,
-    AllowMultiple = true)]              // Allowing multiple instances of attribute
-    public class BugAttribute : Attribute
+    static async Task Main(string[] args)
     {
-        public string Name { get; set; }
-        public string Developer { get; set; }
-        public string Date { get; set; }
-        public BugAttribute() { }
-        public BugAttribute(string n, string dev)
+        // URL of a API
+        string apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Kathmandu&appid=aafba97f9cdd6225a97072d8297ea264";
+
+        try
         {
-            Name = n;
-            Developer = dev;
+            // Perform an asynchronous HTTP GET request
+            string result = await FetchDataAsync(apiUrl);
+
+            Console.WriteLine($"Data from API: {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
-    public class Program
-    {
-        [Bug("Syntax Error", "Alex", Date = "2020-03-5")]
-        public static void Main(string[] args)
-        {
-            var attribute = (BugAttribute)Attribute.GetCustomAttribute(
-            typeof(Program).GetMethod("Main"),
-            typeof(BugAttribute));
 
-            if (attribute != null)
-            {
-                Console.WriteLine($"Date: {attribute.Date}");
-            }
+    static async Task<string> FetchDataAsync(string apiUrl)
+    {
+        using (HttpClient httpClient = new HttpClient())
+        {
+            // Send an asynchronous GET request
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+            // Check if the request was successful
+            response.EnsureSuccessStatusCode();
+
+            // Read and return the content as a string
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
